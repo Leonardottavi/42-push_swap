31626e0f5528e94e9533335898cde73f1b7918e6
Leonardo Ottavi <lottavi@e4r4p2.42roma.it>
2023-06-12 10:55:45 +0200

    inital

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,30 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 10:31:57 by lottavi           #+#    #+#             */
-/*   Updated: 2023/06/12 10:31:58 by lottavi          ###   ########.fr       */
+/*   Updated: 2023/06/12 10:54:15 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+int		swap(t_list **stack);
+int		push(t_list **stack_to, t_list **stack_from);
+int		rotate(t_list **stack);
+int		reverserotate(t_list **stack);
+int		pa(t_list **stack_a, t_list **stack_b);
+int		pb(t_list **stack_a, t_list **stack_b);
+int		rra(t_list **stack_a);
+int		ra(t_list **stack_a);
+int		rb(t_list **stack_b);
+int		rr(t_list **stack_a, t_list **stack_b);
+int		rrb(t_list **stack_b);
+int		rrr(t_list **stack_a, t_list **stack_b);
+int		sa(t_list **stack_a);
+int		sb(t_list **stack_b);
+int		ss(t_list **stack_a, t_list **stack_b);
---
+++ utils/ft_utils.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/12 10:47:54 by lottavi           #+#    #+#             */
+/*   Updated: 2023/06/12 10:54:40 by lottavi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int		count;
+	t_list	*temp;
+
+	count = 0;
+	temp = lst;
+	while (temp->next != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	if (temp->next == NULL)
+		count++;
+	return (count);
+}
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return ;
+	while (s[i++])
+	{
+		ft_putchar_fd(s[i - 1], fd);
+	}
+	ft_putchar_fd('\n', fd);
+}
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return ;
+	while (s[i++])
+	{
+		ft_putchar_fd(s[i - 1], fd);
+	}
+	ft_putchar_fd('\n', fd);
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+int	ft_lstsize(t_list *lst)
+{
+	int		i;
+
+	i = 0;
+	while (lst)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
+}
